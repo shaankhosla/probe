f5607e39706760fd373b51134c669aca5e79d77a
Shaan <shaankhosla@macbook-pro.mynetworksettings.com>
2025-01-05 11:55:11 -0500

    freeze columns, reorg, switch focus

--- cmd/probe.go
+++ cmd/probe.go
@@ -1,69 +1,44 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/gdamore/tcell/v2"
 	_ "github.com/marcboeker/go-duckdb"
-	"github.com/rivo/tview"
 	"probe/cmd/query"
 	"probe/cmd/tui"
 )
 
 const ()
 
-func initializeViews(table *tview.Table, columns *tview.TextView, filename string) {
+func initializeViews(TUI tui.TUI, filename string) {
 	results, err := query.ExecuteSQL("select * from data", filename)
 	if err == nil {
-		tui.UpdateTable(table, results)
+		tui.UpdateTable(TUI, results)
 	}
 
 	columnText := query.GetAllColumns(filename)
-	columns.SetText(columnText).ScrollToBeginning().SetBorder(true).SetTitle("Columns")
+	TUI.Columns.SetText(columnText).ScrollToBeginning().SetBorder(true).SetTitle("Columns")
 }
 
 func RunProbe(filename string) error {
-	app := tview.NewApplication()
-
-	resultsTable := tview.NewTable().
-		SetBorders(true)
-
-	resultsTable.SetTitle(filename)
-	columnsTextView := tview.NewTextView().
-		SetWrap(true).
-		SetTextAlign(tview.AlignLeft)
-
-	initializeViews(resultsTable, columnsTextView, filename)
-	columnsBoxWithContent := tview.NewFlex().
-		AddItem(columnsTextView, 0, 1, false)
-
-	errorTextView := tview.NewTextView().
-		SetWrap(true).
-		SetDynamicColors(true).
-		SetTextAlign(tview.AlignLeft)
-
-	var inputField *tview.TextArea
-	var inner_flex *tview.Flex
-
-	inputField = tview.NewTextArea().SetLabel("Query: ").SetOffset(1, 1)
-	inputField.SetText("select * from data", true)
-	inputField.SetTitle("Query").SetBorder(true).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	TUI := tui.CreateTUIAssets(filename)
+	initializeViews(*TUI, filename)
+	TUI.InputField.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 
 		key := event.Key()
+		if key == tcell.KeyTab {
+			if TUI.InputField.HasFocus() {
+				TUI.App.SetFocus(TUI.ResultsTable)
+			}
+			return nil
+		}
 		if key == tcell.KeyEnter {
 
-			query_text := inputField.GetText()
+			query_text := TUI.InputField.GetText()
 			results, err := query.ExecuteSQL(query_text, filename)
 			if err == nil {
-				inner_flex.ResizeItem(errorTextView, 0, 0)
-				inner_flex.ResizeItem(resultsTable, 0, 10)
-				tui.UpdateTable(resultsTable, results)
-				errorTextView.Clear()
+				tui.UpdateTable(*TUI, results)
 			} else {
-				resultsTable.Clear()
-				inner_flex.ResizeItem(resultsTable, 0, 0)
-				inner_flex.ResizeItem(errorTextView, 0, 10)
-				errorTextView.SetText(fmt.Sprintf("Error: %s", err.Error())).ScrollToBeginning()
+				tui.ShowError(*TUI, err)
 			}
 
 			return nil
@@ -72,22 +47,7 @@ func RunProbe(filename string) error {
 
 	})
 
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyCtrlC {
-			app.Stop()
-			return nil
-		}
-		return event
-	})
-	inner_flex = tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(inputField, 0, 3, true).
-		AddItem(errorTextView, 0, 0, false).
-		AddItem(resultsTable, 0, 10, false)
-	flex := tview.NewFlex().
-		AddItem(columnsBoxWithContent, 0, 1, false).
-		AddItem(inner_flex, 0, 5, true)
-
-	if err := app.SetRoot(flex, true).SetFocus(inputField).Run(); err != nil {
+	if err := TUI.App.Run(); err != nil {
 		panic(err)
 	}
 	return nil
--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -2,6 +2,10 @@ package tui
 
 import (
 	_ "github.com/marcboeker/go-duckdb"
+
+	"fmt"
+	"github.com/gdamore/tcell/v2"
+	_ "github.com/marcboeker/go-duckdb"
 	"github.com/rivo/tview"
 )
 
@@ -10,17 +14,102 @@ const (
 	defaultRows = 100
 )
 
-func UpdateTable(table *tview.Table, rows [][]string) {
-	table.Clear()
+type TUI struct {
+	App          *tview.Application
+	ResultsTable *tview.Table
+	Columns      *tview.TextView
+	Error        *tview.TextView
+	InputField   *tview.TextArea
+	InnerFlex    *tview.Flex
+	OuterFlex    *tview.Flex
+}
+
+func ShowError(TUI TUI, err error) {
+	TUI.ResultsTable.Clear()
+	TUI.InnerFlex.ResizeItem(TUI.ResultsTable, 0, 0)
+	TUI.InnerFlex.ResizeItem(TUI.Error, 0, 10)
+	TUI.Error.SetText(fmt.Sprintf("Error: %s", err.Error())).ScrollToBeginning()
+}
+func UpdateTable(TUI TUI, rows [][]string) {
+	TUI.InnerFlex.ResizeItem(TUI.Error, 0, 0)
+	TUI.InnerFlex.ResizeItem(TUI.ResultsTable, 0, 10)
+	TUI.Error.Clear()
+
+	TUI.ResultsTable.Clear()
 
 	for rowIndex, row := range rows {
 		for colIndex, cell := range row {
-			table.SetCell(rowIndex, colIndex,
+			TUI.ResultsTable.SetCell(rowIndex, colIndex,
 				tview.NewTableCell(cell).
 					SetAlign(tview.AlignCenter).
 					SetSelectable(rowIndex != 0).SetMaxWidth(maxColSize),
 			)
 		}
 	}
-	table.ScrollToBeginning()
+	TUI.ResultsTable.ScrollToBeginning()
+	TUI.ResultsTable.SetFixed(1, 0)
+}
+
+func CreateTUIAssets(filename string) *TUI {
+	app := tview.NewApplication()
+
+	resultsTable := tview.NewTable().
+		SetBorders(true)
+
+	resultsTable.SetTitle(filename)
+	columnsTextView := tview.NewTextView().
+		SetWrap(true).
+		SetTextAlign(tview.AlignLeft)
+
+	columnsBoxWithContent := tview.NewFlex().
+		AddItem(columnsTextView, 0, 1, false)
+
+	errorTextView := tview.NewTextView().
+		SetWrap(true).
+		SetDynamicColors(true).
+		SetTextAlign(tview.AlignLeft)
+
+	var inputField *tview.TextArea
+	var innerFlex *tview.Flex
+
+	inputField = tview.NewTextArea().SetLabel("Query: ").SetOffset(1, 1)
+	inputField.SetText("select * from data", true)
+	inputField.SetTitle("Query").SetBorder(true)
+
+	resultsTable.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		key := event.Key()
+		if key == tcell.KeyTab {
+			if resultsTable.HasFocus() {
+				app.SetFocus(inputField)
+			}
+			return nil
+		}
+		return event
+
+	})
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyCtrlC {
+			app.Stop()
+			return nil
+		}
+		return event
+	})
+	innerFlex = tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(inputField, 0, 3, true).
+		AddItem(errorTextView, 0, 0, false).
+		AddItem(resultsTable, 0, 10, false)
+	flex := tview.NewFlex().
+		AddItem(columnsBoxWithContent, 0, 1, false).
+		AddItem(innerFlex, 0, 5, true)
+
+	app.SetRoot(flex, true).SetFocus(inputField)
+	return &TUI{
+		App:          app,
+		ResultsTable: resultsTable,
+		Columns:      columnsTextView,
+		Error:        errorTextView,
+		InputField:   inputField,
+		InnerFlex:    innerFlex,
+		OuterFlex:    flex,
+	}
 }
